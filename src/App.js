3a1588b89ae299a9968af8cd3cb454759e29b5a0
Abner Jimenez <71862339+abnerjg@users.noreply.github.com>
2023-11-08 14:39:26 -0600

    modulos

--- src/App.js
+++ src/App.js
@@ -4,7 +4,24 @@ import Testimonio from './componentes/Testimonio';
 function App() {
   return (
     <div className="App">
-      <Testimonio />
+      <div className='contenedor-principal'>
+        <h1>Aplicaciones mas usadas actualmente</h1>
+        <Testimonio
+          nombre='Gmail'
+          imagen='gmail'
+          cargo='Asistente'
+          descripcion='Envio correos nacionales e internacionales, Envio correos nacionales e internacionales,Envio correos nacionales e internacionales,Envio correos nacionales e internacionales' />
+        <Testimonio
+          nombre='Whatsapp'
+          imagen='whatsapp'
+          cargo='Comuncacion'
+          descripcion='Envio mensajes personalizados, Envio mensajes personalizados, Envio mensajes personalizados, Envio mensajes personalizados, Envio mensajes personalizados, Envio mensajes personalizados, Envio mensajes personalizados, Envio mensajes personalizados, Envio mensajes' />
+        <Testimonio
+          nombre='Zoom'
+          imagen='zoom'
+          cargo='Virtual'
+          descripcion='Programo y doy reuniones virtuales, Programo y doy reuniones virtuales, Programo y doy reuniones virtuales, Programo y doy reuniones virtuales,Programo y doy reuniones virtuales,' />
+      </div>
     </div>
   );
 }
--- src/componentes/Testimonio.jsx
+++ src/componentes/Testimonio.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
+import '../hojas-de-estilo/Testimonio.css';
 
 function Testimonio(props) {
     return (
         <div className='contenedor-testimonio'>
           <img 
           className='imagen-contenedor'
-          src={require('../imagenes/gmail.png')}
+          src={require(`../imagenes/${props.imagen}.png`)}
           alt='Foto de gmail'/> 
           <div className='contenedor-texto-testimonio'>
-            <p className='nombre-testimonio'>Gmail</p>
-            <p className='cargo-testimonio'>Envio Mensajes</p>
-            <p className='descripcion-testimonio'>Ayudo a enviar correos a miles de usuarios</p>
+            <p className='nombre-testimonio'>{props.nombre}</p>
+            <p className='cargo-testimonio'>{props.cargo}</p>
+            <p className='descripcion-testimonio'>"{props.descripcion}"</p>
           </div>
         </div>
     );
